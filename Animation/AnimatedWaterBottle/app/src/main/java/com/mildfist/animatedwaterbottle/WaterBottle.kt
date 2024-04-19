5cf22619f73d0b475bfdd6471b30b2aeb15d38e7
SANDY <nnal0256@naver.com>
2024-04-20 00:13:37 +0900

    :lipstick: Add Amount Text

--- Animation/AnimatedWaterBottle/app/src/main/java/com/mildfist/animatedwaterbottle/WaterBottle.kt
+++ Animation/AnimatedWaterBottle/app/src/main/java/com/mildfist/animatedwaterbottle/WaterBottle.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
@@ -15,8 +17,12 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mildfist.animatedwaterbottle.ui.theme.Purple40
 
 @Composable
@@ -127,6 +133,33 @@ fun WaterBottle(
                 topLeft = Offset(size.width / 2-capWidth/ 2f, 0f),
                 cornerRadius = CornerRadius(45f, 45f)
             )
+        }
+
+        val text = buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(
+                    color = if(waterPercentage > 0.5f) bottleColor else waterColor,
+                    fontSize = 44.sp
+                )
+            ) {
+                append(usedWaterAmount.toString())
+            }
+            withStyle(
+                style = SpanStyle(
+                    color = if(waterPercentage > 0.5f) bottleColor else waterColor,
+                    fontSize = 44.sp
+                )
+            ) {
+                append(" ")
+                append(unit)
+            }
+        }
+
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = text)
         }
     }
 }
